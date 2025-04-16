13a3de859dccff70481e0e77049df9db0f7495a3
Akash <hafejmohammadakash@gmail.com>
2025-04-16 18:37:18 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <>
+    <section className='w-11/12 mx-auto'>
+    <Header />
+
+    </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            {/* navbar logo */}
+            <div></div>
+
+            {/* banner menu */}
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Fixture</li>
+                    <li>Teams</li>
+                    <li>Schedules</li>
+                </ul>
+            </div>
+
+            {/* show add coin button */}
+            <button></button>
+        </div>
+    );
+};
+
+export default Header;
